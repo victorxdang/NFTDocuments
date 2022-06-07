3b9068ffa6e5ea4f6efa156e532dc32f6ff1840c
Victor Dang <victorxdang@gmail.com>
2022-06-06 22:15:37 -0500

    Modified solidity contract

--- contract.sol
+++ contract.sol
@@ -3,32 +3,30 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
 contract File is ERC721Full {
-    using Counters for Counters.Counter;
-    Counter.Counter private _tokenIds;
 
-    constructor () public ERC721Full("FileToken", "FT"){}
+    constructor () public ERC721Full("FileToken", "FT") {}
 
     struct Document{
         string name; // File name
-        string image; // ipsfs hash of the file
-        string owner; // owner of the file
+        address owner; // owner of the file
     }
+
     mapping(uint256 => Document) public documentCollection;
 
-    event DocumentAdded(uint256 tokenId, string name, string image, string owner, string reportURI);
+    //event DocumentAdded(string name, string owner, string reportURI);
 
     function mintDocument (string memory name,
-                            string memory image, 
                             address owner,
-                            uint256 tokenId,
-                            string memory tokenURI) public returns (uint256)
+                            string memory uri) public returns (uint256)
                             {
-                                uint256 tokenId = _tokenIds.current();
+                                uint256 tokenId = totalSupply();
 
                                 _mint(owner, tokenId);
 
-                                _setTokenURI(tokenId, tokenURI);
+                                _setTokenURI(tokenId, uri);
 
-                                documentCollection[tokenId] = Document(name, image, owner);
+                                documentCollection[tokenId] = Document(name, owner);
+
+                                return tokenId;
                             }
 }
